52f59bfa23ff9899a465d0841606d876f0e93a00
Jozef <Jozefw@sbcglobal.net>
2022-06-12 07:55:12 -0700

    reducer practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React,{useState,useReducer} from 'react';
 import './App.css';
 
 function App() {
+
+  const [] = useReducer()
+  const [counter,setCounter] = useState(0);
+
+
+
+  function increment() {
+    setCounter(counter + 1)
+  }
+  function decrement() {
+    setCounter(counter -1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span onClick={increment}>+</span>
+      <span>{counter}</span>
+      <span onClick={decrement}>-</span>
     </div>
   );
 }
