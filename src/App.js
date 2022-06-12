f270ce50bc5d21c4a9b8f52cd443f406a14b9121
Jozef Woroniecki <jozefw@sbcglobal.net>
2022-06-12 23:20:56 +0000

    add dispatch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React,{useState,useReducer} from 'react';
+import Form from './Form';
 import './App.css';
+
+
+
+const ACTIONS = {
+  INCREMENT:'increment',
+  DECREMENT:'decrement'
+}
+
+
 
 function App() {
 
-  const [] = useReducer()
+  const reducer = (countState,action) =>{
+    switch(action.type){
+      case ACTIONS.INCREMENT:
+        return{count:countState.count+1};
+      case ACTIONS.DECREMENT:
+        return{count:countState.count - 1};
+      default:
+        return {countState}  
+    }
+  } 
+
+  const [countState,dispatch] = useReducer(reducer,{count:0})
   const [counter,setCounter] = useState(0);
 
 
 
   function increment() {
-    setCounter(counter + 1)
+    dispatch({type:increment})
   }
   function decrement() {
-    setCounter(counter -1)
+    dispatch(decrement)
   }
   return (
     <div className="App">
