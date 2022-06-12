e20f7f38920bc16aa11f1c45a16031628afe5549
Jozef Woroniecki <jozefw@sbcglobal.net>
2022-06-12 23:40:27 +0000

    add dispatch and reducer

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import React,{useState,useReducer} from 'react';
+import React,{useReducer} from 'react';
 import Form from './Form';
 import './App.css';
 
 
-
-const ACTIONS = {
-  INCREMENT:'increment',
-  DECREMENT:'decrement'
-}
-
-
-
-function App() {
-
-  const reducer = (countState,action) =>{
-    switch(action.type){
-      case ACTIONS.INCREMENT:
-        return{count:countState.count+1};
+function reducer(countState,action){
+  switch(action.type){
+    case ACTIONS.INCREMENT:
+      return{count:countState.count+1};
       case ACTIONS.DECREMENT:
         return{count:countState.count - 1};
-      default:
-        return {countState}  
-    }
-  } 
+        default:
+          return {countState}  
+        }
+      } 
+      
+      const ACTIONS = {
+        INCREMENT:'increment',
+        DECREMENT:'decrement'
+      }
+function App() {
 
   const [countState,dispatch] = useReducer(reducer,{count:0})
-  const [counter,setCounter] = useState(0);
-
-
 
   function increment() {
-    dispatch({type:increment})
+    dispatch({type:ACTIONS.INCREMENT})
   }
   function decrement() {
-    dispatch(decrement)
+    dispatch({type:ACTIONS.DECREMENT})
   }
   return (
     <div className="App">
       <span onClick={increment}>+</span>
-      <span>{counter}</span>
+      <span>{countState}</span>
       <span onClick={decrement}>-</span>
+      poo
     </div>
   );
 }
